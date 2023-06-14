3b8988cf39197782564e8a1bfabd097bfb4b7dd2
nuguntu <nskhiereli@gmail.com>
2023-06-14 11:21:48 +0400

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import store from "./store/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    {/* <Provider store={store}> */}
     <Routes>
       <Route path="/*" element={<App />} />
     </Routes>
-    {/* </Provider> */}
   </BrowserRouter>
 );
